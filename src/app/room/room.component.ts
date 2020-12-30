3a101c45f2368145a6cc9b857e9adcbeba000eaa
MOONYAN <c8763sasuke@ntut.org.tw>
2020-12-30 21:56:44 +0800

    implement receive msg via socketio

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -3,27 +3,30 @@ import { SendMessageDto } from './dto/send-message.dto';
 import { UserService } from './user.service';
 import { NewedMessage } from './model/newed-message.vm';
 import { RoomService } from './room.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { map, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.css']
 })
-export class RoomComponent implements OnInit {
+export class RoomComponent implements OnInit, OnDestroy {
 
-  messages: Message[] ;
+  messages: Message[] = [];
+  private destory$ = new Subject();
 
   constructor(
     private roomService: RoomService,
     private userService: UserService) { }
 
-
   ngOnInit(): void {
-    this.messages = [
-      new Message({name:'Ninja', text:"Init room"}),
-      new Message({name:'Ninja', text:"Enjoy messaging"}),
-    ];
+    this.messages.push(
+      new Message({ name: 'Ninja', text: "Init room" }),
+      new Message({ name: 'Ninja', text: "Enjoy messaging" })
+    );
+    this.receiveMessage();
   }
 
   sendMessage(newedMessage: NewedMessage) {
@@ -33,5 +36,18 @@ export class RoomComponent implements OnInit {
       text: newedMessage.text
     } as SendMessageDto;
     this.roomService.send(dto);
+  }
+
+  private receiveMessage() {
+    this.roomService.receive().pipe(
+      map(dto => new Message(dto)),
+      takeUntil(this.destory$)
+    ).subscribe(
+      msg => this.messages.push(msg)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.destory$.next();
   }
 }
--- src/app/room/room.service.ts
+++ src/app/room/room.service.ts
@@ -1,14 +1,26 @@
+import { Observable } from 'rxjs';
+import { ReceiveMessageDto } from './dto/receive-message.dto';
 import { SendMessageDto } from './dto/send-message.dto';
 import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
 
-  constructor() { }
+  constructor(private socket: Socket) { }
 
   send(dto: SendMessageDto) {
+    this.socket.emit('chatToServer', dto)
     console.log(`Room service send: ${dto}`);
+  }
+
+  receive(): Observable<ReceiveMessageDto> {
+    return new Observable(observer => {
+      this.socket.on('chatToClient', (message: ReceiveMessageDto) => {
+        observer.next(message);
+      })
+    });
   }
 }
