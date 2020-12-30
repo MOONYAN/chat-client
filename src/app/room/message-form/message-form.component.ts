14243ed3cc5c288fa5b0264b2a78a9fda9b43394
MOONYAN <c8763sasuke@ntut.org.tw>
2020-12-30 19:27:54 +0800

    add component for input msg

---
+++ src/app/room/message-form/message-form.component.ts
@@ -0,0 +1,39 @@
+import { NewedMessage } from './../model/newed-message.vm';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-message-form',
+  templateUrl: './message-form.component.html',
+  styleUrls: ['./message-form.component.css']
+})
+export class MessageFormComponent implements OnInit {
+
+  messageForm: FormGroup;
+
+  @Output() $newedMessage = new EventEmitter<NewedMessage>();
+
+  private readonly initFormValue = {
+    text: ''
+  };
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.messageForm = this.formBuilder.group(this.initFormValue);
+  }
+
+  onSubmit(value) {
+    if (this.messageForm.valid) {
+      const newedMessage = new NewedMessage();
+      newedMessage.text = value.text;
+      this.$newedMessage.emit(newedMessage);
+      this.messageForm.reset(this.initFormValue);
+    }
+  }
+
+  onReset() {
+    this.messageForm.reset(this.initFormValue);
+  }
+
+}
