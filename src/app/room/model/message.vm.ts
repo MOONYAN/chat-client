4769cff66300fea9ab659ea11c4024cef7a8207d
MOONYAN <c8763sasuke@ntut.org.tw>
2020-12-30 19:27:22 +0800

    add component msg-list

---
+++ src/app/room/model/message.vm.ts
@@ -0,0 +1,10 @@
+import { ReceiveMessageDto } from './../dto/receive-message.dto';
+export class Message{
+  name:string;
+  text:string;
+
+  constructor(dto:ReceiveMessageDto){
+    this.name = dto.name;
+    this.text = dto.text;
+  }
+}
