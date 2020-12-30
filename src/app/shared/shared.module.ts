f736548eed1adfafe8814184a14b425046c86ca4
MOONYAN <c8763sasuke@ntut.org.tw>
2020-12-30 17:03:15 +0800

    extend shared module matList

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
@@ -22,6 +23,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatSidenavModule,
     MatCardModule,
     MatGridListModule,
+    MatListModule,
     MatFormFieldModule,
     MatInputModule,
     MatDividerModule,
