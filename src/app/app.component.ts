8c9ec9d1dc6bb33cd8b349df535b6483dfbec226
MOONYAN <c8763sasuke@ntut.org.tw>
2020-12-31 18:41:39 +0800

    update app component and route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { Profile } from './user/model/profile.vm';
+import { Observable } from 'rxjs';
+import { UserService } from './user/user.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'chat-client';
+
+  profile$: Observable<Profile>
+
+  constructor(
+    private router: Router,
+    userService: UserService
+  ) {
+    this.profile$ = userService.getProfile();
+  }
+
+  leaveChat() {
+    this.router.navigate(['/user'])
+  }
 }
